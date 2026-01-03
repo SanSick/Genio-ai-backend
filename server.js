5ab1a5cdff48c258baa0f8e07e814e4cfce64a22
SanSick <sanjeevsinghverma91@gmail.com>
2026-01-03 15:46:39 +0530

    changed cron and cros and proxy

--- server.js
+++ server.js
@@ -125,9 +125,10 @@ const corsOptions = {
   origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   credentials: true,
 };
+app.set("trust proxy", 1);
 app.use(cors(corsOptions));
-app.use(json()); //^pass incoming json data
-app.use(urlencoded({ extended: true }));
+app.use(express.json());//^pass incoming json data
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); //^ to pass the cookie automatically
 
 //-------- Routes -------//
